aad107226c042dfa13930718f3fc71ae1c16bc33
kamil <kamil@MacBook-Air-kamil.local>
2025-07-07 12:54:14 +0200

    add shop

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig(({ command }) => ({
-    base: command === 'serve' ? '/' : '/galaktycznepotyczki.pl',
+    base: command === 'serve' ? '/' : '/',
     build: {
         outDir: 'dist'
     }
